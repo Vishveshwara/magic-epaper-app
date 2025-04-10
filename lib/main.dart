f06bfe16750cf44a5c4535098f8bd5c99d6edd0e
Vishveshwara Uthayakumaran <u.vishveshwara@gmail.com>
2025-04-10 19:37:29 +0530

    Enhanced UI and Functionality for the Select Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:magic_epaper_app/provider/image_loader.dart';
+import 'package:magic_epaper_app/provider/display_provider.dart';
 import 'package:provider/provider.dart';
 
-import 'package:magic_epaper_app/view/home_screen.dart';
+import 'package:magic_epaper_app/view/display_selection_screen.dart';
 
 void main() {
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => ImageLoader()),
-    ],
-    child: const MyApp()
-  ));
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (context) => ImageLoader()),
+    ChangeNotifierProvider(create: (context) => DisplayProvider()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -19,12 +18,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Magic Epaper',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: const SelectDisplay(),
-      );
+      title: 'Magic Epaper',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      home: const DisplaySelectionScreen(),
+    );
   }
 }
